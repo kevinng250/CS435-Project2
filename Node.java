2b7ef49545614ed199fa8c26ae69cc7c42f4aed3
kevinng250 <36280682+kevinng250@users.noreply.github.com>
2020-03-30 18:03:41 -0400

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,10 @@
+import java.util.List;
+    class Node{
+        String name;
+        List<Node> children;
+        public Node(String nodeVal){
+            this.name = nodeVal;
+            children = null;
+        }
+    }
+
